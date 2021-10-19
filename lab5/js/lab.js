a0c398e555f81e8b3ce8ded2b47d6c8b65a592ae
justin990627 <justin990627@gmail.com>
2021-10-18 22:51:58 -0700

    lab 5 .js file update

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,25 +1,38 @@
-#content{
-  background-color: #aaaaaa;
+/*
+* Author: Christian Decareau <cdecarea@ucsc.edu> & Justin Juang <jujuang@ucsc.edu>
+* Created: October 18th
+* License: Public Doman
+*/
 
-  min-height: 800px;
-  padding: 30px;
-  margin: 10px;
-  border: solid 5px #0000FF;
-  font-size: 20px;
 
-}
+//Defined Variables
 
-.style{
-  font-family: Verdana;
-}
+make = "Chrysler";
+model = "PT Cruiser";
+color = "Silver";
+year = 2005;
 
-.text{
-  text-align: left;
+age = 2021 - year;
 
-}
+//Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
+//Seperator for second chunk of output
+document.writeln("<br>");
 
-.frame{
-    float: right;
-    clear:right;
-    margin-left: 20px;
-}
+//Second set of defined variables
+makeTwo = "Merceedes Benz";
+modelTwo = "C Class";
+colorTwo = "Silver";
+yearTwo = 2015;
+
+ageTwo = 2021 - yearTwo;
+//Output continued
+document.writeln("Make: " + makeTwo + "<br>");
+document.writeln("Model: " + modelTwo + "<br>");
+document.writeln("Color: " + colorTwo + "<br>");
+document.writeln("Year: " + yearTwo + "<br>");
+document.writeln("Age: " + ageTwo + "<br>");
