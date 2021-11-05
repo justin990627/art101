f997c94e14c40fc599e73a2b337a80bfc48e6480
justin990627 <justin990627@gmail.com>
2021-11-05 01:45:54 -0700

    update lab9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -13,7 +13,7 @@ var outputEl = document.getElementById("output");
 var new1El = document.createElement("p"); //new <p>
 
   // Change the html attribute of new1El to say something new.
-  new1El = "Is this how it works? "; //add text inside <p></p>
+  new1El.innerHTML = "Is this how it works? "; //add text inside <p></p>
 
   console.log(new1El); //prints new1El as appears in <p>
 
@@ -21,22 +21,22 @@ var new1El = document.createElement("p"); //new <p>
 var new2El = document.createElement("p"); //new <p>
 
   // Change the html attribute of new2El to say something else.
-  new2El = "Yes, it is how it works"; //add text inside <p></p>
+  new2El.innerHTML = "Yes, it is how it works"; //add text inside <p></p>
 
   console.log(new2El); //prints new1El as appears in <p>
 
 // Append both new elements one at a time using appendChild() to your output <div>
 
 
-var test2 = document.createTextNode(new1El); //make new1El into text node
-
-  console.log(test2); //test print new1El assigned as textNode
-
-var test3 = document.createTextNode(new2El);
-
-  console.log(test3);
+// var test2 = document.createTextNode(new1El); //make new1El into text node
+//
+//   console.log(test2); //test print new1El assigned as textNode
+//
+// var test3 = document.createTextNode(new2El);
+//
+//   console.log(test3);
 
 // Change the css attributes of at least two elements of your page
-outputEl.appendChild(test2);
+outputEl.appendChild(new1El);
 //properly attach new1El as a textNode to 'output' <div>
-outputEl.appendChild(test3);
+outputEl.appendChild(new2El);
